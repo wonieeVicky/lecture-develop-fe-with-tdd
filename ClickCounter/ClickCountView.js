d20e83734179999c3412cbfdde2475a6c21385c7
Vicky <hwfongfing@gmail.com>
2021-01-14 22:20:22 +0900

    feat: update test code for tdd(clickCountView-2)

--- ClickCounter/ClickCountView.js
+++ ClickCounter/ClickCountView.js
@@ -1,17 +1,21 @@
-var App = App || {}
+var App = App || {};
 
 App.ClickCountView = (clickCounter, updateEl) => {
-  if (!clickCounter) throw new Error(App.ClickCountView.messages.noClickCounter)
-  if (!updateEl) throw new Error(App.ClickCountView.messages.noUpdateEl)
-  
+  if (!clickCounter) throw new Error(App.ClickCountView.messages.noClickCounter);
+  if (!updateEl) throw new Error(App.ClickCountView.messages.noUpdateEl);
+
   return {
     updateView() {
-      updateEl.innerHTML = clickCounter.getValue()
+      updateEl.innerHTML = clickCounter.getValue();
     },
-  }
-}
+    increaseAndUpdateView() {
+      clickCounter.increase();
+      this.updateView();
+    },
+  };
+};
 
 App.ClickCountView.messages = {
-  noClickCounter: 'clickCount를 주입해야 합니다',
-  noUpdateEl: 'updateEl를 주입해야 합니다'
-}
+  noClickCounter: "clickCount를 주입해야 합니다",
+  noUpdateEl: "updateEl를 주입해야 합니다",
+};
--- ClickCounter/ClickCountView.spec.js
+++ ClickCounter/ClickCountView.spec.js
@@ -1,42 +1,45 @@
-describe('App.ClickCountView 모듈', () => {
-  let udpateEl, clickCounter, view
+describe("App.ClickCountView 모듈", () => {
+  let udpateEl, clickCounter, view;
 
-  it('ClickCounter를 주입하지 않으면 에러를 던진다', ()=> {
-    const clickCounter = null
-    const updateEl = document.createElement('span') 
-    const actual = () => App.ClickCountView(clickCounter, updateEl)
-    expect(actual).toThrowError(App.ClickCountView.messages.noClickCounter)
-  })
+  it("ClickCounter를 주입하지 않으면 에러를 던진다", () => {
+    const clickCounter = null;
+    const updateEl = document.createElement("span");
+    const actual = () => App.ClickCountView(clickCounter, updateEl);
+    expect(actual).toThrowError(App.ClickCountView.messages.noClickCounter);
+  });
 
-  it('updateEl를 주입하지 않으면 에러를 던진다', ()=> {
-    const clickCounter = App.ClickCounter()
-    const updateEl = null 
-    const actual = () => App.ClickCountView(clickCounter, updateEl)
-    expect(actual).toThrowError(App.ClickCountView.messages.noUpdateEl)
-  })
+  it("updateEl를 주입하지 않으면 에러를 던진다", () => {
+    const clickCounter = App.ClickCounter();
+    const updateEl = null;
+    const actual = () => App.ClickCountView(clickCounter, updateEl);
+    expect(actual).toThrowError(App.ClickCountView.messages.noUpdateEl);
+  });
 
-  beforeEach(()=> {
-    updateEl = document.createElement('span')
-    clickCounter = App.ClickCounter(); 
-    view = App.ClickCountView(clickCounter, updateEl)
-  })
-  
+  beforeEach(() => {
+    updateEl = document.createElement("span");
+    clickCounter = App.ClickCounter();
+    view = App.ClickCountView(clickCounter, updateEl);
+  });
 
-  describe('updateView()', () => {
-    it('ClickCounter의 getValue() 실행결과를 출력한다', ()=> {
-      const counterValue = clickCounter.getValue()
-      view.updateView()
-      expect(updateEl.innerHTML).toBe(counterValue.toString())
-    })
-  })
+  describe("updateView()", () => {
+    it("ClickCounter의 getValue() 실행결과를 출력한다", () => {
+      const counterValue = clickCounter.getValue();
+      view.updateView();
+      expect(updateEl.innerHTML).toBe(counterValue.toString());
+    });
+  });
 
-  describe('increaseAndUpdateView()는', ()=> {
-    it('ClickCounter의 increase 를 실행한다', ()=> {
-      // todo
-    })
-    
-    it('updateView를 실행한다', ()=> {
-      // todo 
-    })
-  })
-})
+  describe("increaseAndUpdateView()는", () => {
+    it("ClickCounter의 increase 를 실행한다", () => {
+      spyOn(clickCounter, "increase");
+      view.increaseAndUpdateView();
+      expect(clickCounter.increase).toHaveBeenCalled();
+    });
+
+    it("updateView를 실행한다", () => {
+      spyOn(view, "updateView");
+      view.increaseAndUpdateView();
+      expect(view.updateView).toHaveBeenCalled();
+    });
+  });
+});
