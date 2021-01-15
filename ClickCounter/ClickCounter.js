dbc829f3043ee5eeca121d34faaf35aad9e936d7
Vicky <hwfongfing@gmail.com>
2021-01-15 23:33:15 +0900

    feat: 클릭 카운터 모듈 리팩토링 -2

--- ClickCounter/ClickCounter.js
+++ ClickCounter/ClickCounter.js
@@ -1,17 +1,22 @@
-var App = App || {}
+var App = App || {};
 
-App.ClickCounter = _data => {
-  if (!_data) throw Error('_data')
-  const data = _data
-  data.value = data.value || 0
+App.ClickCounter = (_data) => {
+  if (!_data) throw Error("_data");
+  const data = _data;
+  data.value = data.value || 0;
 
   return {
     getValue() {
-      return data.value
+      return data.value;
     },
 
     count() {
-      data.value++
-    }
-  }
-}
+      data.value++;
+    },
+
+    setCountFn(fn) {
+      this.count = () => (data.value = fn(data.value));
+      return this;
+    },
+  };
+};
